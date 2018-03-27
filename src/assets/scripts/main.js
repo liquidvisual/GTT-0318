a4c909e0447f71443764be9fc4023ea05918f41e
Mitch <liquidvisual@gmail.com>
2018-03-27 23:28:00 +1100

    Update

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -26,23 +26,29 @@ $(window).on('load', function() {
 //-----------------------------------------------------------------
 
 function launchGallery(){
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    $('.popup-gallery').each(function(){
+        $this = $(this);
+        groupTitle = $this.attr('data-group-title');
+
+        $this.magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small>'+groupTitle+'</small>';
+                }
             }
-        }
-    });
+        });
+    })
+
 }
 
 //-----------------------------------------------------------------
